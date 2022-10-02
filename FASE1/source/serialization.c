2510c239b1cca5c230b5d649936e40fda2402c50
Francisco Teixeira <franjateixeira@gmail.com>
2022-10-02 16:29:34 +0100

    Finalização de serialization.c

--- FASE1/source/serialization.c
+++ FASE1/source/serialization.c
@@ -11,23 +11,21 @@
  */
 int keyArray_to_buffer(char **keys, char **keys_buf) {
 
-    if(**keys == NULL) {
+    if(**keys == NULL || *keys_buf == NULL) {
         return -1;
     }
 
     int size = htonl(sizeof(keys));
-    int bufferSize = sizeof(int) + sizeof(keys);
+    int bufferSize = sizeof(int) + sizeof(keys) + 1;
     *keys_buf = malloc(bufferSize);
 
     if(*keys_buf == NULL) {
         return -1;
     }
     
-    memcpy(*keys_buf, &size, sizeof(int));
-    int i = 0;
-    while(*keys + i == NULL) {
-        memcpy(*keys_buf + sizeof(int) + sizeof(*keys + i), *keys + i, sizeof(keys));
-    }
+    memcpy(keys_buf, &size, sizeof(int));
+    memcpy(keys_buf+sizeof(int), &keys, bufferSize);
+
     return bufferSize;
 }
 
@@ -36,4 +34,19 @@ int keyArray_to_buffer(char **keys, char **keys_buf) {
  * cujo espaco em memória deve ser reservado. Devolve NULL
  * em caso de erro.
  */
-char** buffer_to_keyArray(char *keys_buf, int keys_buf_size);
+char** buffer_to_keyArray(char *keys_buf, int keys_buf_size){
+
+    if(keys_buf == NULL || keys_buf_size<0){
+        return NULL;
+    }
+
+    char **returnArray = malloc(keys_buf_size);
+
+    if(returnArray == NULL){
+        return NULL;
+    }
+
+    memcpy(returnArray, &keys_buf+sizeof(int), keys_buf_size-sizeof(int));
+
+    return returnArray;
+}
