51ca2bedd392926d5ae37b617b5fac59570a9248
Francisco-Teixeirax86 <74599559+Francisco-Teixeirax86@users.noreply.github.com>
2022-11-11 10:01:15 +0000

    pasta temp para a aula

---
+++ temp-aula/grupo21/source/tree_server.c
@@ -0,0 +1,32 @@
+/*
+Grupo 21:
+	Francisco Teixeira | FC56305
+	Alexandre Rodrigues | FC54472
+	Afonso Soares | FC56314
+*/
+#include "inet.h"
+#include "network_server.h"
+#include "tree_skel.h"
+
+
+struct rtree_t *tree;
+
+int main(int argc, char *argv[]) {
+
+	if(argc != 2) {
+		printf("Certifique-se que executou o programa corretamente, ./serve <port>");
+		printf("\n");
+		return -1;
+	}
+
+	int socket = network_server_init(atoi(argv[1]));
+	if(socket == -1) {
+		printf("Ocorreu um erro ao criar o server");
+	}
+	tree_skel_init();
+	network_main_loop(socket);
+	network_server_close();
+	tree_skel_destroy();
+
+
+}
