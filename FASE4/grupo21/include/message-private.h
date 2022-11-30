064bc1e957e51052b6e76a6b4a9c4b027d58c8af
AlexDRodrigues <80759142+adr189@users.noreply.github.com>
2022-11-30 22:44:46 +0000

    Código da fase 3 para fazer fase 4

---
+++ FASE4/grupo21/include/message-private.h
@@ -0,0 +1,23 @@
+/*
+Grupo 21:
+	Francisco Teixeira | FC56305
+	Alexandre Rodrigues | FC54472
+	Afonso Soares | FC56314
+*/
+#ifndef _MESSAGE_PRIVATE_H
+#define _MESSAGE_PRIVATE_H
+
+#include "sdmessage.pb-c.h"
+#include "inet.h"
+
+/*
+ * Função que envia uma string inteira pela rede
+*/
+int write_all(int sock, void *buf, int len);
+
+/*
+ * Função que recebe uma string inteira pela rede
+*/
+int read_all(int sock, void *buf, int len);
+
+#endif
